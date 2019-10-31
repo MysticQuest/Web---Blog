2584edb942836f9f69a0f8bf33d84a1bc9ddfe00
Mystic Quest <l1skou@gmail.com>
2019-10-31 20:28:47 +0200

    initialized app.js, fixed folder structure, added ejs satics

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port: 3000");
+});
